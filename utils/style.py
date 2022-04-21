5ee84b52e9b616aef0d5d3c358b955f72c2cd513
VladTomici14 <vladtomici@gmail.com>
2022-04-21 12:41:43 +0300

    Added the file that contains all the styling parameters.

---
+++ utils/style.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+# --------- styling variables -------
+font = cv2.FONT_HERSHEY_SIMPLEX
+redColor = (0, 0, 255)
+greenColor = (0, 255, 0)
+whiteColor = (255, 255, 255)
+blueColor = (255, 85, 45)
+
+
+# in case you want to change the fonts
+def showAllFonts():
+    blank_image = np.zeros((500, 1000, 3), np.uint8)
+
+    text_to_show = "The quick brown fox jumps over the lazy dog"
+
+    cv2.putText(blank_image,
+                "Hershey Simplex : " + text_to_show,
+                (20, 40),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Plain : " + text_to_show,
+                (20, 80),
+                fontFace=cv2.FONT_HERSHEY_PLAIN,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Duplex : " + text_to_show,
+                (20, 120),
+                fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Complex : " + text_to_show,
+                (20, 160),
+                fontFace=cv2.FONT_HERSHEY_COMPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Triplex : " + text_to_show,
+                (20, 200),
+                fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Complex Small : " + text_to_show,
+                (20, 240),
+                fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Script Simplex : " + text_to_show,
+                (20, 280),
+                fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.putText(blank_image,
+                "Hershey Script Complex : " + text_to_show,
+                (20, 320),
+                fontFace=cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+                fontScale=1,
+                color=(255, 255, 255))
+
+    cv2.imshow("Fonts", blank_image)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    showAllFonts()
